ef77dd023dfff2eb2c7930fb5427f026f326905a
marcos-stefane <65078339+marcos-stefane@users.noreply.github.com>
2020-05-11 22:12:55 -0300

    versão inicial

---
+++ js/placar.js
@@ -0,0 +1,57 @@
+function inserePlacar(){
+
+    var corpoTabela = $(".placar").find("tbody");
+    var usuario = "Seu-nome";
+    var numPalavras = $("#contador-palavras").text();
+
+    var linha = novaLinha(usuario,numPalavras);
+    linha.find(".botao-remover").click(removeLinha);
+
+    corpoTabela.append(linha);
+    $(".placar").slideDown(500);
+    scrollPlacar();
+}
+function novaLinha(usuario,numPalavras){
+
+    var linha = $("<tr>");
+    var colunaUsuario = $("<td>").text(usuario);
+    var colunaPalavras = $("<td>").text(numPalavras);
+    var colunaRemover = $("<td>");
+
+    var link = $("<a>").attr("href","#").addClass("botao-remover");
+    var icone = $("<i>").addClass("small").addClass("material-icons").text("delete");
+
+    link.append(icone);
+    colunaRemover.append(link);
+
+    linha.append(colunaUsuario);
+    linha.append(colunaPalavras);
+    linha.append(colunaRemover);
+
+    return linha;
+}
+function removeLinha(event){
+
+    event.preventDefault();
+    var linha = $(this).parent().parent();
+
+    linha.fadeOut(1000);
+    setTimeout(function() {
+        linha.remove();
+    },1000);
+}
+$("#botao-placar").click(mostraPlacar);
+
+function mostraPlacar(){
+
+    $(".placar").stop().toggle(600);
+
+}
+function scrollPlacar() {
+
+    var posicaoPlacar = $(".placar").offset().top;  
+    $("body").animate(
+        {
+            scrollTop:posicaoPlacar + "px"
+        },1000);
+}
